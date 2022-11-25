77917ba853a598c79ed67a5bbf07013667c4fb35
baltatescuvalentin <baltatescuvalentin14@gmail.com>
2022-11-25 12:51:09 +0200

    Just for learning.

---
+++ index.js
@@ -0,0 +1,44 @@
+let http = require('http');
+let fs = require('fs');
+
+http.createServer(
+    (req, res) => {
+        if(req.url === "/") {
+            fs.readFile('index.html', (err, data) => {
+                if(err) console.log(`Something went wrong: ${err}`);
+                res.writeHead(200, { 'Content-Type': 'text/html'})
+                res.write(data);
+                res.end();
+            })
+        }
+        else if(req.url === "/about") {
+            fs.readFile('about.html', (err, data) => {
+                if(err) console.log(`Something went wrong: ${err}`);
+                res.writeHead(200, { 'Content-Type': 'text/html'})
+                res.write(data);
+                res.end();
+            })
+        }
+        else if(req.url === "/contact-me") {
+            fs.readFile('contact-me.html', (err, data) => {
+                if(err) console.log(`Something went wrong: ${err}`);
+                res.writeHead(200, { 'Content-Type': 'text/html'})
+                res.write(data);
+                res.end();
+            })
+        }
+        else {
+            fs.readFile('404.html', (err, data) => {
+                if(err) console.log(`Something went wrong: ${err}`);
+                res.writeHead(200, { 'Content-Type': 'text/html'})
+                res.write(data);
+                res.end();
+            })
+        }
+    }
+).listen(8080, (err) => {
+    if(err)
+        console.log(`Something went wrong: ${err}`);
+    else
+        console.log('Listening on port 8080')
+});
